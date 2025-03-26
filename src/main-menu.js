415c76aaa3705568c56ccd488e1a0a189e633d64
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2025-03-26 15:43:23 -0400

    fix: platform pieces having gap
    For some reason, 2 pieces don't span enough to cover the whole screen
    when implemented as a carousel, even though the width of each platform
    should be 1/2 of the screen. I just added another piece, which "solved"
    the problem.

--- src/main-menu.js
+++ src/main-menu.js
@@ -2,9 +2,10 @@ import { k } from './kaplay_context.js';
 
 export default function mainMenu() {
 
-    const gameSpeed = 400
+    const gameSpeed = 300
 
   if (!k.getData('best-score')) k.setData('best-score', 0);
+  console.log()
 
   k.onButtonPress('jump', () => k.go('game'));
   const bgPieceWidth = 1920;
@@ -13,10 +14,13 @@ export default function mainMenu() {
     k.add([k.sprite('chemical-bg'), k.pos(bgPieceWidth, 0), k.scale(2), k.opacity(0.8)]),
   ];
 
-  const platformWidth = 1280;
+  const platformWidth = 1080;
+  const platformHeight = 700
   const platforms = [
-    k.add([k.sprite('platforms'), k.pos(0, 700)], k.scale(4)),
-    k.add([k.sprite('platforms'), k.pos(platformWidth, 900)], k.scale(4)),
+    k.add([k.sprite('platforms'), k.pos(0, platformHeight)], k.scale(8,2)),
+    k.add([k.sprite('platforms'), k.pos(platformWidth, 900)], k.scale(8,2)),
+    k.add([k.sprite('platforms'), k.pos(platformWidth*2, 900)], k.scale(8,2)),
+
   ];
 
   k.onUpdate(() => {
@@ -28,19 +32,24 @@ export default function mainMenu() {
     bgPieces[1].moveTo(bgPieces[0].pos.x + bgPieceWidth * 2, 0);
 
     if (platforms[1].pos.x < 0) {
-      platforms[0].moveTo(platforms[1].pos.x+platformWidth, 200);
+      platforms[0].moveTo(platforms[1].pos.x+platformWidth*4,platformHeight);
       platforms.push(platforms.shift());
     }
+    // if (platforms[0].pos.x < 400) {
+    //     platforms[2].moveTo(platforms[0].pos.x+platformWidth*4, 200);
+    //     platforms.push(platforms.shift());
+    //   }
+    // if (platforms[0].pos.x < 0) {
+    //     platforms[1].moveTo(platforms[1].pos.x+platformWidth*4, 200);
+    //     // platforms.push(platforms.shift());
+    //   }
 
     platforms[0].move(-gameSpeed, 0);
-    platforms[1].moveTo(platforms[0].pos.x + platformWidth,500)
-    // platforms.push(platforms.shift());
+    platforms[1].moveTo(platforms[0].pos.x + platformWidth+200,platformHeight);
+    platforms[2].moveTo(platforms[1].pos.x + platformWidth+200,platformHeight);
     
-
-    // platforms[0].move(-gameSpeed, 0);
-    // platforms[1].moveTo(platforms[0].pos.x + platformWidth, 700);
-
-    // platforms.forEach(platform=>
-    // platform.move(-500, 0),platforms)
+    // platforms[0].moveTo(platforms[1].pos.x+platformWidth, 200);
+    
+    
   });
 }
